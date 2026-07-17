2d58c9e692bb5ff6e55e1d6688d8b5a9f35ae9e3
mahmudhany18 <mahmudhany18@gmail.com>
2026-07-17 14:30:22 +0200

    Update audio.js

--- JS/audio.js
+++ JS/audio.js
@@ -1,40 +1,56 @@
-const music = document.getElementById("bgMusic");
+const audio = document.getElementById("bgMusic");
 
-if (music) {
+const playlist = [
+    "music/gh.mp3",
+    "music/s1.mp3",
+    "music/s2.mp3",
+    "music/s3.mp3",
+    "music/s4.mp3",
+    "music/s5.mp3",
+    "music/s6.mp3",
+    "music/s7.mp3",
+    "music/s8.mp3",
+    "music/s9.mp3",
+    "music/s10.mp3"
+];
 
-    // Restore previous position
-    const savedTime = localStorage.getItem("musicTime");
+let currentSong =
+    parseInt(localStorage.getItem("currentSong")) || 0;
 
-    if (savedTime) {
-        music.currentTime = Number(savedTime);
-    }
+audio.src = playlist[currentSong];
 
+const savedTime = localStorage.getItem("musicTime");
 
-    // Save current position every second
-    setInterval(() => {
-
-        localStorage.setItem(
-            "musicTime",
-            music.currentTime
-        );
-
-    }, 1000);
-
-
-    // When song ends, start again
-    music.addEventListener("ended", () => {
-
-        music.currentTime = 0;
-        music.play();
-
-    });
-
-
-    // Try to continue automatically
-    music.play().catch(() => {
-
-        console.log("Waiting for user interaction");
-
-    });
-
+if (savedTime) {
+    audio.currentTime = parseFloat(savedTime);
 }
+
+audio.play().catch(() => {});
+
+setInterval(() => {
+    localStorage.setItem("musicTime", audio.currentTime);
+}, 1000);
+
+audio.addEventListener("ended", () => {
+
+    let next;
+
+    do {
+        next = Math.floor(Math.random() * playlist.length);
+    } while (next === currentSong);
+
+    currentSong = next;
+
+    localStorage.setItem("currentSong", currentSong);
+
+    audio.src = playlist[currentSong];
+
+    audio.currentTime = 0;
+
+    audio.play();
+
+});
+
+window.addEventListener("beforeunload", () => {
+    localStorage.setItem("musicTime", audio.currentTime);
+});
